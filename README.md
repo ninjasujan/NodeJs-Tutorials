72b15530e801c449b86bb1b72270d2cfc74c4ce0
sujan <sujanpoojary884@gmail.com>
2019-12-11 04:11:12 +0530

    server created and respone made

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # NodeJs-Tutorials
+
+# This course is all about Nodejs 
+
+# This is just a reference notes, what I have practised in nodejs While I taking this course.
---
+++ nodeJs-Basic/server.js
@@ -0,0 +1,46 @@
+
+// importing module
+const http = require('http');
+const fs = require('fs');
+
+// creating server unsing http module
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+            <htmL>
+            <head><title>NodeJs Basic</title></head>
+            <body>
+                <form action="/message" method="POST">
+                    <input type="text" name="message">
+                    <button type="submit">Send Message</button>
+                </form>
+            </body>
+            </html>
+        `); 
+        return res.end();
+    }
+    if(req.url === '/message' && req.method === 'POST') {
+        const body = [];
+        let message = null;
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            message = parsedBody.split('=')[1];
+            console.log(message);
+            fs.writeFile('someText.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });   
+        });
+        
+        
+    }
+});
+
+// listening to port 
+server.listen(4000);
